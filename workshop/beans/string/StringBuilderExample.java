9405af9195ee7cf0add9b35b67f018e7a9406389
keerthika146 <keerthikar24cse@vetias.ac.in>
2025-07-07 15:42:58 +0530

    String

---
+++ workshop/beans/string/StringBuilderExample.java
@@ -0,0 +1,17 @@
+package workshop.beans.string;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("java ");
+        sb.append("is ");
+        sb.append("the ");
+        sb.append("most ");
+        sb.append("used ");
+        sb.append("language ");sb.append("for ");
+        sb.append("enterprise ");
+        sb.append("application ");
+        System.out.println(sb.toString());
+    }
+
+}
