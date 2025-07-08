993591f85c7d59d75b4c6503bfb32827195c7fed
keerthika146 <keerthikar24cse@vetias.ac.in>
2025-07-08 15:31:58 +0530

    calculator

---
+++ workshop/Calculator.java
@@ -0,0 +1,10 @@
+package workshop;
+
+public class Calculator {
+    public void main(String[] args) {
+        Calculator calculator=new Calculator();
+        System.out.println("Division:" + calculator.divide(6,0));
+        System.out.println("Addition:" + calculator.add(5,3));
+        System.out.println("Subtraction:" + calculator.subtract(10,5));
+          }
+}
