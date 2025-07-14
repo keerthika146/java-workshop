9e05f21c9ad396cc9b09252420f7af14362118b7
keerthika146 <keerthikar24cse@vetias.ac.in>
2025-07-14 15:00:08 +0530

    organitation

---
+++ workshop/beans/Organaization.java
@@ -0,0 +1,9 @@
+package workshop.beans;
+public record Organaization(String name,
+                String completeAddress,
+                String website,
+                String email,
+                String contactNumber,
+                Long registrationNumber) {
+
+}
